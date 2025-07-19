80a0454a3ba960fedd5eaedea22ecdc74c196136
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-07-18 21:29:35 -0300

    Create script.js

---
+++ Exercícios/Basico/exec004/script.js
@@ -0,0 +1,12 @@
+// Exercício 4: Faça uma soma entre dois números e concatenação entre duas strings.
+
+let a = 7;
+let b = 4;
+let c = a + b;
+
+console.log(`${a} + ${b} = ${c}`);
+
+let string1 = "eu sou";
+let string2 = "o goat maximo";
+
+console.log(string1 + " " + string2)
