d9fbbe3fa67542138d5df4ea67ae9c45c35f34c5
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-07-18 21:29:31 -0300

    Create script.js

---
+++ Exercícios/Basico/exec003/script.js
@@ -0,0 +1,18 @@
+// Exercício 3: Converta uma string em número e vice-versa.
+
+let strinNum = "123";
+
+let num = Number(strinNum);
+
+console.log(typeof strinNum);
+console.log(typeof num);
+
+let numString = 12;
+
+let string = numString.toString();
+
+console.log("\n\n" + typeof numString);
+console.log(typeof string);
+
+
+
