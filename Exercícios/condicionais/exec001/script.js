d64274482cb9d5eb8903d70e6bcf1e3e8e6d9852
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-07-18 21:44:28 -0300

    Create script.js

---
+++ Exercícios/condicionais/exec001/script.js
@@ -0,0 +1,12 @@
+// Exercício 1: Verifique se uma pessoa pode dirigir (idade >= 18).
+
+let pessoa = 18;
+
+console.log("verificando se você pode dirigir")
+
+if(pessoa >= 18){
+    console.log("\nVocê pode dirigir");
+}
+else {
+    console.log("\nVocê não pode digirir");
+}
