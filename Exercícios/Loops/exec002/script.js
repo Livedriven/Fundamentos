96303790ed8584a9437fe78b40c4d5bece9ddb5b
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-07-20 12:31:26 -0300

    Create script.js

---
+++ Exercícios/Loops/exec002/script.js
@@ -0,0 +1,9 @@
+// Exercício 2: Imprima todos os números pares até 50 com while.
+
+let i = 0;
+
+while(i <= 50) // não ira para ate chegar em 50
+{
+    console.log(i + "\n");
+    i += 2; // vai aumentando de 2 em 2
+}
