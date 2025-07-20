4a65eec805d0899d74bfa944df1ee4e93bff8d5c
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-07-20 12:31:22 -0300

    Create script.js

---
+++ Exercícios/Loops/exec001/script.js
@@ -0,0 +1,6 @@
+// Loops Exercício 1: Liste números de 1 a 100 com for.
+
+for(let i = 1; i <= 100; i ++) // mandando os for pecorrer a partir do 1 e chegar ate o 100
+{
+    console.log(i + "\n");
+}
